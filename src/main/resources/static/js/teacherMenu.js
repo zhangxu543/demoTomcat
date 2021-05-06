d07612589a446e1d800f601994c7637d28e6e453
张旭 <15075131893@zrgj.com>
2021-05-06 22:18:42 +0800

    tomcat项目

---
+++ src/main/resources/static/js/teacherMenu.js
@@ -0,0 +1,47 @@
+var nemu = {
+	"data": [{
+			"path": "1",
+			"title": "用户管理",
+			"icon": "../asserts/img/icons/yonghuguanli.png",
+			"children": [{
+				"path": "/1-1",
+				"title": "用户列表",
+				"linkUrl": "",
+				"children": []
+			}]
+		},
+		{
+			"path": "2",
+			"title": "学生管理 ",
+			"icon": "../asserts/img/icons/yonghuguanli.png",
+			"children": [{
+				"path": "/2-1",
+				"title": "学生列表",
+				"linkUrl": "/teacherStudents",
+				"children": []
+			},{
+				"path": "/2-2",
+				"title": "奖罚列表",
+				"linkUrl": "/awarePunishes",
+				"children": []
+			},{
+				"path": "/2-3",
+				"title": "成绩列表",
+				"linkUrl": "/grades",
+				"children": []
+			}
+			]
+		},
+		{
+			"path": "3",
+			"title": "个人信息管理",
+			"icon": "../asserts/img/icons/yonghuguanli.png",
+			"children": [{
+				"path": "/3-1",
+				"title": "个人信息",
+				"linkUrl": "/teacher/findById",
+				"children": []
+			}]
+		}
+	]
+}
